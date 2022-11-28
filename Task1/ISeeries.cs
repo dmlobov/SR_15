2d200da16bc7b754a4b0a5ca9c099b6524acf061
D L <d.m.lobov@yandex.ru>
2022-11-28 16:22:08 +0300

    Добавьте файлы проекта.

---
+++ Task1/ISeeries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    interface ISeries
+    {
+        void setStar(int x);
+        int getNext();
+        void reset();
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Арифметическая прогрессия");
+            ArithProgression arith = new ArithProgression();
+            arith.setStar(10);
+            arith.setStep(2);
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine("Сброс значений");
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine("Геометрическая прогрессия");
+            GeomProgression geom = new GeomProgression();
+            geom.setStar(5);
+            geom.setStep(3);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine("Сброс значений");
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
